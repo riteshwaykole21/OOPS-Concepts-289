d773be9bf8311ae4112a188b9a15332d8af22b22
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-24 09:31:26 +0530

    [Ritesh]add : Method OverLoading Concepts

---
+++ OOPSDemo/MethodOverLoading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    public class MethodOverLoading
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("Without Parameter");
+        }
+        public void MethodOne(int num)
+        {
+            Console.WriteLine($"With One Parameter : Num = {num}");
+        }
+        public void MethodOne(int num ,string str)
+        {
+            Console.WriteLine($"With Two Parameter : Num = {num} , string = {str}");
+        }
+        public void MethodOne(string str , int num)
+        {
+            Console.WriteLine($"With Two Parameter : Num = {num} , string = {str}");
+        }
+        public void MethodOne(string Name, string str)
+        {
+            Console.WriteLine($"With Two Parameter : string = {Name} , string = {str}");
+        }
+    }
+}
--- OOPSDemo/Program.cs
+++ OOPSDemo/Program.cs
@@ -13,6 +13,7 @@ namespace OOPSDemo
             Console.WriteLine("Welcome in OOPS Concepts");
             Console.WriteLine("1 : Class And Object");
             Console.WriteLine("2 : Inheritance Concept");
+            Console.WriteLine("3 : Method OverLoading Concepts");
             Console.WriteLine("Enter A Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -25,6 +26,14 @@ namespace OOPSDemo
                     LastName last = new LastName();
                     last.Info("Ritesh");
                     last.Infoo("Waykole");
+                    break;
+                case 3:
+                    MethodOverLoading method = new MethodOverLoading();
+                    method.MethodOne();
+                    method.MethodOne(30);
+                    method.MethodOne(50,"Concepts");
+                    method.MethodOne("Concepts" ,50);
+                    method.MethodOne("MethodOverloading","Concepts");
                     break;
             }
             Console.ReadLine();
