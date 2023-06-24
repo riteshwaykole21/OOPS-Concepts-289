454ef89451ecb175a249a45d538f65f13807de03
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-24 08:20:58 +0530

    [Ritesh]add : Class And Objectn Concepts

---
+++ OOPSDemo/ClassObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    public class ClassObject
+    {
+        public void Methods()
+        {
+            Console.WriteLine("Enter First Number");
+            int First = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int Second = Convert.ToInt32(Console.ReadLine());
+            int Sum = First + Second;
+            Console.WriteLine($"The Sum of Two Number is {Sum}");
+        }
+    }
+}
---
+++ OOPSDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in OOPS Concepts");
+            ClassObject classObject = new ClassObject();
+            classObject.Methods();
+            Console.ReadLine();
+        }
+    }
+}
