2db323a610b2f9d055a006db8a29d2fd09acd9f0
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-24 08:58:33 +0530

    [Ritesh]add : Inheritances Concepts

---
+++ OOPSDemo/FirstName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    public class FirstName
+    {
+        public void Info(string name)
+        {
+            Console.WriteLine($"My First Name is {name}");
+        }
+    }
+    public class LastName : FirstName
+    {
+        public void Infoo(string lastname)
+        {
+            Console.WriteLine($"My Last Name is {lastname}");
+        }
+    }
+}
--- OOPSDemo/Program.cs
+++ OOPSDemo/Program.cs
@@ -11,8 +11,22 @@ namespace OOPSDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome in OOPS Concepts");
-            ClassObject classObject = new ClassObject();
-            classObject.Methods();
+            Console.WriteLine("1 : Class And Object");
+            Console.WriteLine("2 : Inheritance Concept");
+            Console.WriteLine("Enter A Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    ClassObject classObject = new ClassObject();
+                    classObject.Methods();
+                    break;
+                case 2:
+                    LastName last = new LastName();
+                    last.Info("Ritesh");
+                    last.Infoo("Waykole");
+                    break;
+            }
             Console.ReadLine();
         }
     }
