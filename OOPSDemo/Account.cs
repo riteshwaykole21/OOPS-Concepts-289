d512e02f38e24ec6f6c0f353cbd5a8952c20c4b8
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-26 07:24:56 +0530

    [Ritesh]add : Encapsulation Concepts

---
+++ OOPSDemo/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    internal class Account
+    {
+        int accountBalances = 1000;
+        public void SetBalance(int Amount)
+        {
+            accountBalances = Amount;   
+        }
+        public void GetBalances()
+        {
+            Console.WriteLine($"Your Account Balances is {accountBalances}");
+        }
+    }
+}
--- OOPSDemo/Program.cs
+++ OOPSDemo/Program.cs
@@ -15,6 +15,7 @@ namespace OOPSDemo
             Console.WriteLine("2 : Inheritance Concept");
             Console.WriteLine("3 : Method OverLoading Concepts");
             Console.WriteLine("4 : Method Overriding Concepts");
+            Console.WriteLine("5 : Encapsulation Concepts");
             Console.WriteLine("Enter A Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -39,6 +40,11 @@ namespace OOPSDemo
                 case 4:
                     Dog d = new Dog();
                     d.Eating();
+                    break;
+                case 5:
+                    Account account = new Account();
+                    account.SetBalance(100);
+                    account.GetBalances();
                     break;
             }
             Console.ReadLine();
